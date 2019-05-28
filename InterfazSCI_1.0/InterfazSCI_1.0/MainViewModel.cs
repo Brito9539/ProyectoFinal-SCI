11da99019c87480e99b2dd58fe39b8fa341faff7
Brito <brito97@live.com>
2019-05-28 12:35:29 -0500

    PrimerCoso
    Inicio del proyecto

---
+++ InterfazSCI_1.0/InterfazSCI_1.0/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfazSCI_1._0
+{
+    class MainViewModel
+    {
+        ObservableCollection<object> _children;
+        public MainViewModel()
+        {
+            _children = new ObservableCollection<object>();
+            _children.Add(new EntradaViewModel());
+        }
+
+        public ObservableCollection<object> Children { get { return _children; } }
+    }
+}
---
+++ InterfazSCI_1.0/InterfazSCI_1.0/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InterfazSCI_1._0
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Style s = new Style();
+            s.Setters.Add(new Setter(UIElement.VisibilityProperty, Visibility.Collapsed));
+            tcmenu.ItemContainerStyle = s;
+
+        }
+
+        private void BtnEntradas_Click(object sender, RoutedEventArgs e)
+        {
+            tcmenu.SelectedIndex = 0;
+        }
+
+        private void BtnSalidas_Click(object sender, RoutedEventArgs e)
+        {
+            tcmenu.SelectedIndex = 1;
+        }
+
+        private void BtnArticulos_Click(object sender, RoutedEventArgs e)
+        {
+            tcmenu.SelectedIndex = 2;
+        }
+
+        private void BtnAgregarArt_Click(object sender, RoutedEventArgs e)
+        {
+            tcmenu.SelectedIndex = 3;
+        }
+
+      
+        private void BtnAgregarUsu_Click(object sender, RoutedEventArgs e)
+        {
+            tcmenu.SelectedIndex = 6;
+        }
+
+        private void BtnUsuarios_Click(object sender, RoutedEventArgs e)
+        {
+            tcmenu.SelectedIndex = 7;
+        }
+
+        private void BtnAgregarPro_Click(object sender, RoutedEventArgs e)
+        {
+            tcmenu.SelectedIndex = 9;
+        }
+
+        private void BtnUsuario_Click(object sender, RoutedEventArgs e)
+        {
+            tcmenu.SelectedIndex = 5;
+        }
+
+        private void BtnProveedores_Click(object sender, RoutedEventArgs e)
+        {
+            tcmenu.SelectedIndex = 8;
+        }
+    }
+}
