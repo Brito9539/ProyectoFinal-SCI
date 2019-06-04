f5043d8fe4c9c20c285a3f02ebbe744b2d154c22
Adrianooow96 <Adrianooow96@users.noreply.github.com>
2019-06-04 11:33:09 -0500

    A

---
+++ InterfazSCI_1.0/SCI.Windows/ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.ObjectModel;
+
+namespace SCI.Windows
+{
+    public abstract class ViewModel : ObservableObject, IDataErrorInfo
+    {
+        public string this[string columnName] => OnValidate(columnName);
+         
+        public string Error => throw new NotSupportedException();
+
+        protected virtual string OnValidate(string propertyName)
+        {
+            var context = new ValidationContext(this)
+            {
+                MemberName = propertyName
+            };
+
+            var results = new Collection<ValidationResult>();
+            var isValid = Validator.TryValidateObject(this, context, results, true);
+
+            return !isValid ? results[0].ErrorMessage : null;
+        }
+    }
+}
